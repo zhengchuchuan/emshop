add6a6903e67f483cbd518e08d1e4bf142dd899d
ZhengChuchuan <1531379706@qq.com>
2025-06-11 22:53:57 +0800

    user server data层接口设计

--- internal/app/user/srv/data/v1/user.go
+++ internal/app/user/srv/data/v1/user.go
@@ -3,11 +3,39 @@ package v1
 import (
 	"context"
 	metav1 "emshop/pkg/common/meta/v1"
+	"time"
+
+	"gorm.io/gorm"
 )
 
+type BaseModel struct {
+	ID        int32     `gorm:"primarykey"`
+	CreatedAt time.Time `gorm:"column:add_time"`
+	UpdatedAt time.Time `gorm:"column:update_time"`
+	DeletedAt gorm.DeletedAt
+	IsDeleted bool
+}
+
+
+/*
+1. 密文 2. 密文不可反解
+ 1. 对称加密
+ 2. 非对称加密
+ 3. md5 信息摘要算法
+    密码如果不可以反解，用户找回密码
+*/
 type UserDO struct {
-	// dv1.UserDO
-	Name string `json:"name"` //用户名
+	BaseModel
+	Mobile   string     `gorm:"index:idx_mobile;unique;type:varchar(11);not null"`
+	Password string     `gorm:"type:varchar(100);not null"`
+	NickName string     `gorm:"type:varchar(20)"`
+	Birthday *time.Time `gorm:"type:datetime"`
+	Gender   string     `gorm:"column:gender;default:male;type:varchar(6) comment 'female表示女, male表示男'"`
+	Role     int        `gorm:"column:role;default:1;type:int comment '1表示普通用户, 2表示管理员'"`
+}
+
+func (u *UserDO) TableName() string {
+	return "user"
 }
 
 type UserDOList struct {
@@ -15,11 +43,11 @@ type UserDOList struct {
 	Items      []*UserDO `json:"data"`                 //数据
 }
 
-func List(ctx context.Context, opts metav1.ListMeta) (*UserDOList, error) {
+// func List(ctx context.Context, opts metav1.ListMeta) (*UserDOList, error) {
 
 
-	return &UserDOList{}, nil
-}
+// 	return &UserDOList{}, nil
+// }
 
 
 type UserStore interface {
@@ -30,15 +58,15 @@ type UserStore interface {
 	//用户列表
 	List(ctx context.Context, opts metav1.ListMeta) (*UserDOList, error)
 
-	// //通过手机号码查询用户
-	// GetByMobile(ctx context.Context, mobile string) (*UserDO, error)
+	//通过手机号码查询用户
+	GetByMobile(ctx context.Context, mobile string) (*UserDO, error)
 
-	// //通过用户ID查询用户
-	// GetByID(ctx context.Context, id uint64) (*UserDO, error)
+	//通过用户ID查询用户
+	GetByID(ctx context.Context, id uint64) (*UserDO, error)
 
-	// //创建用户
-	// Create(ctx context.Context, user *UserDO) error
+	//创建用户
+	Create(ctx context.Context, user *UserDO) error
 
-	// //更新用户
-	// Update(ctx context.Context, user *UserDO) error
+	//更新用户
+	Update(ctx context.Context, user *UserDO) error
 }
